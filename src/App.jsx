d15d403b68a98323c330287d086a6fad35e72180
joaromasodun <joaromasodun@gmail.com>
2025-04-25 08:36:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Features from "./sections/Features";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
+
+
+export default function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing/>
+    </main>
+  )
+}
